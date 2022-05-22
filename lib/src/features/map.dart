c8081c743ff8aac8eab436e95d1ddf5b0a60c7cc
北洞亜也加 <pocchi13.tr@gmail.com>
2022-05-22 16:25:49 +0900

    map 調整

--- lib/src/features/map.dart
+++ lib/src/features/map.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
@@ -26,7 +28,8 @@ class Map extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final selectedCountry = ref.watch(countryProvider.state);
-    final contries = useState<List<CountryModel>>([]);
+    final countries = useState<List<CountryModel>>([]);
+    final collectionNumber = useState(0);
     List<CountryModel> _data = <CountryModel>[
       CountryModel('Brazil', -14.235004, -51.92528, 0),
       CountryModel('Germany', 51.16569, 10.451526, 0),
@@ -47,13 +50,12 @@ class Map extends HookConsumerWidget {
       var counts = await CollectionDB.hasCountCountry(db);
       _data.asMap().forEach((index, element) {
         var country = element.country;
-        print(country);
         var getCount = counts.firstWhereOrNull((item) => item.country == country);
         if (getCount != null) {
-          print(getCount.count);
-          contries.value.add(CountryModel(element.country, element.latitude, element.longitude, getCount.count));
+          collectionNumber.value += getCount.count;
+          countries.value.add(CountryModel(element.country, element.latitude, element.longitude, getCount.count));
         } else {
-          contries.value.add(element);
+          countries.value.add(element);
         }
         _controller.insertMarker(index);
       });
@@ -65,35 +67,96 @@ class Map extends HookConsumerWidget {
 
     return Scaffold(
       appBar: AppBar(
-        title: Text(""),
+        title: const Text("Select"),
       ),
-      body: Center(
-        child: SfMaps(layers: <MapLayer>[
-          MapShapeLayer(
-            source: _dataSource,
-            initialMarkersCount: contries.value.length,
-            markerBuilder: (BuildContext context, int index) {
-              return MapMarker(
-                latitude: contries.value[index].latitude,
-                longitude: contries.value[index].longitude,
-                child: GestureDetector(
-                  onTap: () {
-                    print("${index}: onTap called.");
-                    selectedCountry.state = contries.value[index].country;
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => const MovieList()));
+      body: Stack(
+          fit: StackFit.passthrough,
+          children: [
+            Positioned(
+              top: 0,
+              width: MediaQuery.of(context).size.width,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 25, bottom: 20),
+                      child: Text(
+                        "${collectionNumber.value}/${countries.value.length}",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                    ),
+                    Wrap(
+                     children: countries.value.map((country) => _label(country)).toList(),
+                    ),
+                  ]
+              )
+            ),
+            Positioned(
+              bottom: 0,
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width * 0.95,
+              child: SfMaps(layers: <MapLayer>[
+                MapShapeLayer(
+                  source: _dataSource,
+                  initialMarkersCount: countries.value.length,
+                  color: Colors.blueGrey[100],
+                  strokeColor: Colors.blueGrey[230],
+                  markerBuilder: (BuildContext context, int index) {
+                    return MapMarker(
+                      latitude: countries.value[index].latitude,
+                      longitude: countries.value[index].longitude,
+                      child: GestureDetector(
+                        onTap: () {
+                          selectedCountry.state = countries.value[index].country;
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => const MovieList()));
+                        },
+                        child: Icon(
+                            countries.value[index].count > 0 ? Icons.where_to_vote : Icons.flag,
+                            color: countries.value[index].count > 0 ? Colors.pink.shade400.withOpacity(0.8) : Colors.teal.shade400.withOpacity(0.8),
+                            size: 40,
+                        ),
+                      ),
+                    );
                   },
-                  child: Icon(
-                      Icons.add_location,
-                      color: contries.value[index].count > 0 ? Colors.red : Colors.grey,
-                      size: 30,
+                  selectionSettings: MapSelectionSettings(
+                    color: Colors.orange,
+                    strokeColor: Colors.red[900],
+                    strokeWidth: 3,
                   ),
+                  controller: _controller,
                 ),
-              );
-            },
-            controller: _controller,
-          ),
-        ],),
+              ],
+              ),
+            )
+          ]
       ),
+    );
+  }
+
+  Widget _label(CountryModel country) {
+    String text = '${country.country}: ${country.count}';
+    return Container(
+        margin: const EdgeInsets.all(3),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.circular(20.0)
+        ),
+        child: Padding(
+          padding: const EdgeInsets.only(top: 5, bottom: 5, right: 10, left: 10),
+          child: Text(
+            text,
+            style: const TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 15,
+              color: Colors.blueGrey,
+            ),
+          ),
+        ),
     );
   }
 }
