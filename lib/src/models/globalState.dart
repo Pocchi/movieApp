012f1c6658af84effb56d18666eafbb5654b3893
北洞亜也加 <pocchi13.tr@gmail.com>
2022-05-22 17:00:26 +0900

    realm 削除

--- lib/src/models/globalState.dart
+++ lib/src/models/globalState.dart
@@ -1,10 +1,8 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:movie/src/realmModels/collection.dart';
 
 enum TabType { map, collectionList }
 final tabTypeProvider = StateProvider<TabType>((ref) => TabType.map);
 final countryProvider = StateProvider((ref) => 'Japan');
-final collectionProvider = StateProvider((ref) => CollectionModel());
 
 
 
